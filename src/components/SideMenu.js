6cf9ef812b99e8dfcc4dd8be9841b54f55d1b7b3
josue rojas <josuerojas.rojas@gmail.com>
2018-03-19 19:33:13 -0400

    made language buttons functional

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -15,7 +15,7 @@ class LanguageSquare extends Component {
         float: 'left',
         border: '1px solid rgba(31, 31, 31, 0.8)',
         fontWeight: '400',
-        fontSize: '.95rem',
+        fontSize: '.9rem',
         letterSpacing: '.1rem',
         cursor: 'pointer',
       },
@@ -28,29 +28,53 @@ class LanguageSquare extends Component {
   }
 }
 
+class DotButton extends Component {
+  render() {
+    return(
+      <div className={'DotButton-wrapper ' + this.props.isActive} onClick={this.props.handleClick}>
+        <div className='DotButton'>
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>
+
+    )
+  }
+}
+
 export default class SideMenu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: 'Featured Projects'
+      active: false
     }
     this.makeLangSquare = this.makeLangSquare.bind(this);
+    this.dotMenuClick = this.dotMenuClick.bind(this);
   }
 
   makeLangSquare() {
     const langSqu = [];
     langSqu.push(
       <LanguageSquare
-        language={'All'}/>
+        language={'All'}
+        onClick={this.props.languageSquareClick}/>
     )
     for(let i = 0; i < this.props.languages.length; i++){
       langSqu.push(
         <LanguageSquare
-          language={this.props.languages[i]}/>
+          language={this.props.languages[i]}
+          onClick={this.props.languageSquareClick}/>
       )
 
     }
     return(langSqu)
+  }
+
+  dotMenuClick(){
+    this.setState({
+      active: !this.state.active
+    })
   }
 
   render() {
@@ -77,7 +101,6 @@ export default class SideMenu extends Component {
         letterSpacing: '.1rem',
         cursor: 'pointer',
         width: '100%',
-        // marginBottom: '25px',
         backgroundColor: 'rgba(31, 31, 31, .6)',
       },
       title: {
@@ -95,10 +118,15 @@ export default class SideMenu extends Component {
 
     }
     return(
-      <div style={style.menu} className='SideMenu'>
-        <a href='/#' style={style.home}>Home</a>
-        <div style={style.laguages}>
-          {this.makeLangSquare()}
+      <div>
+        <DotButton
+          isActive={this.state.active}
+          handleClick={this.dotMenuClick}/>
+        <div style={style.menu} className={'SideMenu ' + this.state.active}>
+          <a href='/#' style={style.home}>Home</a>
+          <div style={style.laguages}>
+            {this.makeLangSquare()}
+          </div>
         </div>
       </div>
     )
